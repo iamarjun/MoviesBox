2824fdd47c300caab7b0adcf7b27c348f84f6273
iamarjun <arjunmanoj1995@gmail.com>
2020-10-13 23:39:00 +0530

    add viewbinding delegation class and switch to viewbinding

--- app/src/main/java/com/arjun/moviesbox/ui/home/HomeFragment.kt
+++ app/src/main/java/com/arjun/moviesbox/ui/home/HomeFragment.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.arjun.moviesbox.R
+import com.arjun.moviesbox.databinding.FragmentHomeBinding
+import com.arjun.moviesbox.util.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
+    private val binding: FragmentHomeBinding by viewBinding(FragmentHomeBinding::bind)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,10 +27,14 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            binding.textHome.text = it
+        })
     }
 }
